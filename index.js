3df12186be86f42787667878db90c24959e1ca2f
EvePulido <epulido3@ucol.mx>
2023-10-04 08:50:12 -0600

    Primera versión

---
+++ index.js
@@ -0,0 +1,21 @@
+// Declaración de variables en Javascript moderno
+// let numero = 5;
+// numero = 8;
+// const constante = 4;
+
+// Alerta en Javascript
+// alert('Hola desde una alerta de javascript');
+// console.log('Esto es javascript desde el HTML');
+// console.log(numero);
+// console.log(constante);
+
+// Concatenación
+let nombre = 'Evelyn';
+let apellido = 'Pulido';
+console.log('Mi nombre es: '+nombre+' '+apellido);
+
+// document.write('Hola en el documento desde js');
+
+let seleccion = document.querySelector('#seccion1');
+
+seleccion.innerHTML = `<h3>Mi nombre es</h3>`;
